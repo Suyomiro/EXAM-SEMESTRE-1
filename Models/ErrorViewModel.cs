9a0287c19c257df62c1c63a5944a68d70739b3c9
KHALIQ <152598763+Suyomiro@users.noreply.github.com>
2025-12-19 07:51:17 +0000

    Ajout du projet ASP.NET MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Brasil_Burger_Client.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
