d57a51c4f5d8461932b8e5e34da226da0b33ae21
KHALIQ <152598763+Suyomiro@users.noreply.github.com>
2025-12-19 12:01:13 +0000

    Configure Render port and HTTPS redirection

--- Program.cs
+++ Program.cs
@@ -3,19 +3,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Forcer l'application à écouter sur le port Render
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseRouting();
+// ⚠️ HTTPS redirection uniquement si ce n’est pas en développement
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -24,6 +31,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
