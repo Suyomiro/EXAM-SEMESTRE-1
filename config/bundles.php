90d0398ee668798db7c851172dadc2e9ba92e19f
KHALIQ <152598763+Suyomiro@users.noreply.github.com>
2025-12-30 23:04:15 +0000

    bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -11,7 +11,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 
-    // Dev / Test seulement
+    // Uniquement pour le développement
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
